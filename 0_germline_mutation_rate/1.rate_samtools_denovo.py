2c06139005f593280caec85f6b4ae54999c2cb55
Devan Desai <devandesai@trentu.ca>
2025-05-01 17:55:05 -0400

    Update 1.rate_samtools_denovo.py - format for my directories - generalized

--- 0_germline_mutation_rate/1.rate_samtools_denovo.py
+++ 0_germline_mutation_rate/1.rate_samtools_denovo.py
@@ -2,38 +2,40 @@
 """
 This script identifies in the new variant calling the true de novo candidates and the false positive ones.
 Lucie Bergeron
+    Devan Desai
 03.12.18
 """
-##################################################
-# What you need ##################################
-##################################################
+#################
+# What you need #
+#################
 
 # Packages:
 import subprocess
 import os
+from variable import *
 import pandas as pd
 
 # Import species names:
 species = pd.read_csv('sp_here.txt', sep=' ', index_col=None, header=None)
 
-##################################################
-# What you run  ##################################
-##################################################
+################
+# What you run #
+################
 # For each chromosome one function:
 for sp in range(0,len(species)):
     sp_target=species.loc[sp,0]
     print(sp_target)
     # Directories:
-    direct_denovo="/home/lucie/MammalianMutation/faststorage/{}/de_novo_mutation/".format(sp_target)
+    direct_denovo="{}/{}/de_novo_mutation/".format(path, sp_target)
     direct_here="/home/lucie/MammalianMutation/pipeline_NGS/playing_around/re_run_denovo/{}/".format(sp_target)
-    direct = "/home/lucie/MammalianMutation/faststorage/{}/".format(sp_target)
+    direct = "{}/{}/".format(path, sp_target)
     # Dictionary:
     pedigree_sp=pd.read_csv('{}/pedigree.ped'.format(direct), sep=' ', index_col=None, header=None)
     for sample in range(0,len(pedigree_sp)):
         off = pedigree_sp.iloc[sample,2]
         print(off)
         denovo_to_check=pd.read_csv('{}data_denovo_{}.tab'.format(direct_denovo, off), sep='\t', index_col=None)
-        denovo_samtools=pd.read_csv('{}{}_samtools.txt'.format(direct_here, off), sep='\t', index_col=None, header=None)
+        denovo_samtools=pd.read_csv('{}{}_samtools.txt'.format(direct_denovo, off), sep='\t', index_col=None, header=None)
         idx_pass = []
         idx_nov = []
         idx_parentv = []
@@ -71,12 +73,10 @@ for sp in range(0,len(species)):
                 data_idk=data_idk.append(denovo_to_check.iloc[denovo])
                 print('idk')
         nb_denovo=len(data_new)
-        file = open('{}denovo.txt'.format(direct_here),'a')
+        file = open('{}denovo.txt'.format(direct_denovo),'a')
         file.write('{} {} \n'.format(off,nb_denovo))
         file.close()
-        data_new.to_csv('{}data_denovo_{}.tab'.format(direct_here, off), sep='\t')
-        data_nov.to_csv('{}data_denovo_{}_nov.tab'.format(direct_here, off), sep='\t')
-        data_parentv.to_csv('{}data_denovo_{}_parentv.tab'.format(direct_here, off), sep='\t')
-        data_idk.to_csv('{}data_denovo_{}_idk.tab'.format(direct_here, off), sep='\t')
-
-
+        data_new.to_csv('{}data_denovo_{}.tab'.format(direct_denovo, off), sep='\t')
+        data_nov.to_csv('{}data_denovo_{}_nov.tab'.format(direct_denovo, off), sep='\t')
+        data_parentv.to_csv('{}data_denovo_{}_parentv.tab'.format(direct_denovo, off), sep='\t')
+        data_idk.to_csv('{}data_denovo_{}_idk.tab'.format(direct_denovo, off), sep='\t')
